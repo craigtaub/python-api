ad8c6195d53ef7f333c308e8087e4df827e0fe76
Craig Taub <craigtaub@gmail.com>
2017-09-27 15:12:42 +0100

    Add spec

--- test/test_mapped.py
+++ test/test_mapped.py
@@ -8,9 +8,11 @@ class TestMapped(unittest.TestCase):
         pass
 
     def test_first(self):
+        """Test 12 returns 1200"""
         self.assertEqual(process(12), 1200)
 
     def test_second(self):
+        """Test 13 returns 1300"""
         self.assertEqual(process(13), 1300)
 
 if __name__ == '__main__':
