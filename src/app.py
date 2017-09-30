e51fa8f82668512b0ccc6bc0f21c481fcb33b247
Craig Taub <craigtaub@gmail.com>
2017-09-30 14:20:51 +0100

    return JSON

--- src/app.py
+++ src/app.py
@@ -1,19 +1,38 @@
 # pylint: disable=C0111
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from mapped import process
+import logging
 
 APP = Flask(__name__)
 
+
+# Hello
 @APP.route("/")
 def hello():
     return "Hello World!"
 
+
+# Basic template + variable
 @APP.route("/route1")
 def route1():
     return render_template('hello.html', name="Craig")
 
-@APP.route('/post/<int:post_id>')
-def show_post(post_id): # integer
+
+# Process url param and return
+@APP.route('/post/<int:post_id>') # integer
+def show_post(post_id):
     mapped_it = process(post_id)
     return 'Post %d' % mapped_it
+
+# Process url param, log, JSON return it
+@APP.route('/api/<int:post_id>')
+def api(post_id):
+    
+    mapped_it = process(post_id)
+    logging.warning('Log to terminal post_id %d', post_id) # log it
+
+    return jsonify(
+        id=mapped_it,
+        key="value"
+    )
