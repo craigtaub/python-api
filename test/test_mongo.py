40d72126c4786e493ec0794739d674bec76f4094
Craig Taub <craigtaub@gmail.com>
2017-11-01 15:26:57 +0000

    add async test

--- test/test_mongo.py
+++ test/test_mongo.py
@@ -1,23 +1,27 @@
 # pylint: disable=C0111
 
 import unittest
+from unittest.mock import Mock
 from mock import MagicMock, patch, PropertyMock
+import asyncio
 
 class StubObject(object): pass
-
-
-dataDict = {'name': 'NAME DATA'}
-dataList = [dataDict]
+dataList = {'name': 'NAME DATA'}
 findMock = MagicMock()
-findMock.return_value = dataList
+def get_mock_coro(return_value): # required for async testing
+    @asyncio.coroutine
+    def mock_coro(*args, **kwargs):
+        return return_value
+    return Mock(side_effect=findMock, wraps=mock_coro) # assert on side effect of coroutine
 clubs = StubObject()
-clubs.find = findMock
+clubs.find_one = get_mock_coro(dataList) # findMock
 sportsbase = StubObject()
 sportsbase.clubs = clubs
 myObject = StubObject()
 myObject.sportsbase = sportsbase
-class TestMongo(unittest.TestCase):
 
+class TestMongo(unittest.TestCase):
+  
     @patch('service.make_request')
     def test_fetchOne(self, mock):
         from mongo import fetchOne
@@ -27,13 +31,15 @@ class TestMongo(unittest.TestCase):
         self.assertEqual(fetchOne(), "RESPONSE DATA")
         mock.assert_called_with('one')
 
-    @patch('pymongo.MongoClient', return_value=myObject)
+    @patch('motor.motor_asyncio.AsyncIOMotorClient', return_value=myObject)
     def test_fetch(self, mock_dont_call):
         """Should call mongo with expected id and return response"""
         from mongo import fetch
 
-        self.assertEqual(fetch(), "NAME DATA")
-        findMock.assert_called_with({'id': 9160})
+        result = asyncio.get_event_loop().run_until_complete(fetch('12'))
+
+        self.assertEqual(result, "NAME DATA")
+        findMock.assert_called_with({'id': 12})
         self.assertEqual(findMock.call_count, 1)
 
 
@@ -44,7 +50,6 @@ class TestMongo(unittest.TestCase):
         mock.__getitem__.side_effect = lambda name: 'stub'
         result = testMethod()
         self.assertEqual(result, 'stub')
-
 
 if __name__ == '__main__':
     unittest.main() # set so can run in isolation
